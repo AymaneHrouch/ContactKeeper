e462ae71e288255317ca5530b73320dc935f07c1
Aymane Hrouch <jo.aymane@Gmail.com>
2022-06-06 00:30:10 +0000

    make useEffect in Contacts.js run after component did mount

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -11,7 +11,7 @@ const Contacts = () => {
     useEffect(() => {
         contactContext.getContacts();
         // eslint-disable-next-line
-    });
+    }, []);
 
     if (contacts !== null && contacts.length === 0 && !loading) {
         return <h4>Please add a contact</h4>;
@@ -21,8 +21,6 @@ const Contacts = () => {
         return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
     };
 
-    console.log('filtered', filtered);
-    console.log('contacts', contacts);
     let visible_contacts = filtered ? filtered : contacts;
 
     return (
